a46550322416aca09003fd91b1679989d746669c
Rohit <rohitsharma.royal@gmail.com>
2024-07-25 20:46:56 +0530

    feat: anki: Add Parser.java

--- basemodule/src/main/java/org/eu/thedoc/zettelnotes/plugins/base/utils/PatternUtils.java
+++ basemodule/src/main/java/org/eu/thedoc/zettelnotes/plugins/base/utils/PatternUtils.java
@@ -6,18 +6,49 @@ public class PatternUtils {
 
   public enum Regex {
 
-    // [scheduled]: <2023-09-01 09:12 .+1w>
-    // Text
+    /**
+     * [scheduled]: <2023-09-01 09:12 .+1w>
+     * <br>
+     * Text
+     */
     ALARM(Pattern.compile("^\\[scheduled\\]: <(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2})(| (\\+\\d+?\\w))>(\\n|)(^.*$|)",
         Pattern.MULTILINE | Pattern.CASE_INSENSITIVE)),
     RECURRENCE(Pattern.compile("(\\+)(\\d+?)(\\w)", Pattern.CASE_INSENSITIVE)),
-    //0-11	 - [x] task
-    //1. 0-1
-    //2. 4-5	x
-    //3. 7-11	task
+    /**
+     * 0-11	 - [x] task 1. 0-1 2. 4-5	x 3. 7-11	task
+     */
     TASK(Pattern.compile("^([ \\t]*)[-*]\\s\\[([\\sxX])\\]\\s(.+)$")),
     //TodoTxt
-    TodoTxt(Pattern.compile("((x) |)\\((\\w)\\) ((\\d{4}-\\d{2}-\\d{2}) |)((\\d{4}-\\d{2}-\\d{2}) |)(.+)", Pattern.CASE_INSENSITIVE));
+    TodoTxt(Pattern.compile("((x) |)\\((\\w)\\) ((\\d{4}-\\d{2}-\\d{2}) |)((\\d{4}-\\d{2}-\\d{2}) |)(.+)", Pattern.CASE_INSENSITIVE)),
+    //Anki
+    /**
+     * # Style
+     * <br>
+     * This style is suitable for having the header as the front, and the answer as the back
+     */
+    ANKI_HEADER(Pattern.compile("^# (.+?)\\n+([^#]*)\\n{2,}")),
+    /**
+     * Q: How do you use this style?
+     * <br>
+     * A: Just like this.
+     */
+    ANKI_QA(Pattern.compile("^Q: (.+?)\\n+A: (.+?)$")),
+    /**
+     * How do you use ruled style?
+     * <br>
+     * ---
+     * <br>
+     * You need at least three '-' between the front and back of the card.
+     */
+    ANKI_RULED(Pattern.compile("(.+?)\\n-{3,}\\n(.+?)$")),
+    /**
+     * The idea of {cloze paragraph style} is to be able to recognise any paragraphs that contain {cloze deletions}.
+     */
+    ANKI_CLOZE(Pattern.compile("^(.*?)\\{(.+?)\\}(.*?)$")),
+    /**
+     * Find cloze only
+     */
+    ANKI_CLOZE_ONLY(Pattern.compile("\\{(.+?)\\}"));
 
     public final Pattern pattern;
     public final String regex;
---
+++ plugin-btn-anki/src/main/java/org/eu/thedoc/zettelnotes/buttons/anki/Parser.java
@@ -0,0 +1,100 @@
+package org.eu.thedoc.zettelnotes.buttons.anki;
+
+import androidx.annotation.StringDef;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import org.eu.thedoc.zettelnotes.buttons.anki.Parser.Type.Types;
+import org.eu.thedoc.zettelnotes.plugins.base.utils.PatternUtils.Regex;
+
+public class Parser {
+
+  public static List<Card> getCards(String text) {
+    if (text == null || text.isEmpty()) {
+      return new ArrayList<>();
+    }
+
+    List<Card> list = new ArrayList<>();
+    list.addAll(getClozes(text));
+    list.addAll(getRuled(text));
+    list.addAll(getQA(text));
+    list.addAll(getHeader(text));
+    return list;
+  }
+
+  private static List<Card> getClozes(String text) {
+    List<Card> list = new ArrayList<>();
+    Matcher clozeMatcher = Regex.ANKI_CLOZE.pattern.matcher(text);
+    while (clozeMatcher.find()) {
+      //Get all lines with Cloze
+      String group = clozeMatcher.group();
+      //Convert to Anki Format
+      Matcher matcher = Regex.ANKI_CLOZE_ONLY.pattern.matcher(group);
+      StringBuffer stringBuffer = new StringBuffer();
+      int index = 0;
+      while (matcher.find()) {
+        matcher.appendReplacement(stringBuffer, String.format("{{c%s:%s}}", index, matcher.group(1)));
+        index++;
+      }
+      matcher.appendTail(stringBuffer);
+      //Add in list
+      String card = stringBuffer.toString();
+      list.add(new Card(card, "", Type.CLOZE));
+    }
+    return list;
+  }
+
+  private static List<Card> getRuled(String text) {
+    List<Card> list = new ArrayList<>();
+    Matcher clozeMatcher = Regex.ANKI_RULED.pattern.matcher(text);
+    while (clozeMatcher.find()) {
+      //Get all lines with ruled format
+      String group1 = clozeMatcher.group(1);
+      String group2 = clozeMatcher.group(2);
+      //Add in list
+      list.add(new Card(group1, group2, Type.RULED));
+    }
+    return list;
+  }
+
+  private static List<Card> getQA(String text) {
+    List<Card> list = new ArrayList<>();
+    Matcher clozeMatcher = Regex.ANKI_QA.pattern.matcher(text);
+    while (clozeMatcher.find()) {
+      //Get all lines with ruled format
+      String group1 = clozeMatcher.group(1);
+      String group2 = clozeMatcher.group(2);
+      //Add in list
+      list.add(new Card(group1, group2, Type.QA));
+    }
+    return list;
+  }
+
+  private static List<Card> getHeader(String text) {
+    List<Card> list = new ArrayList<>();
+    Matcher clozeMatcher = Regex.ANKI_HEADER.pattern.matcher(text);
+    while (clozeMatcher.find()) {
+      //Get all lines with ruled format
+      String group1 = clozeMatcher.group(1);
+      String group2 = clozeMatcher.group(2);
+      //Add in list
+      list.add(new Card(group1, group2, Type.HEADER));
+    }
+    return list;
+  }
+
+  public static final class Type {
+
+    public static final String CLOZE = "cloze";
+    public static final String HEADER = "header";
+    public static final String QA = "qa";
+    public static final String RULED = "ruled";
+
+    @StringDef({CLOZE, HEADER, QA, RULED})
+    public @interface Types {
+      //
+    }
+  }
+
+  public record Card(String question, String answer, @Types String type) {}
+}
